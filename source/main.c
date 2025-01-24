0de29cb2146e5194c100e642b923ab8257fe0772
0hJah <silkolim@gmail.com>
2025-01-24 23:22:47 +0400

    init

---
+++ source/main.c
@@ -0,0 +1,55 @@
+// Include the most common headers from the C standard library
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Include the main libnx system header, for Switch development
+#include <switch.h>
+
+// Main program entrypoint
+int main(int argc, char* argv[])
+{
+    nsInitialize();
+    consoleInit(NULL);
+
+    // Configure our supported input layout: a single player with standard controller styles
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
+    PadState pad;
+    padInitializeDefault(&pad);
+
+    NsApplicationRecord appRecords[1024];
+    s32 actualAppRecordCnt = 0;
+    Result res;
+    NsApplicationControlData *buf=NULL;
+    u64 outsize=0;
+    res = nsListApplicationRecord(appRecords, 1024, 0,  &actualAppRecordCnt);
+
+    appletRequestLaunchApplication(appRecords[rand()% actualAppRecordCnt+1].application_id, NULL);
+
+
+    // Main loop
+    while (appletMainLoop())
+    {
+        // Scan the gamepad. This should be done once for each frame
+        padUpdate(&pad);
+
+        // padGetButtonsDown returns the set of buttons that have been
+        // newly pressed in this frame compared to the previous one
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus)
+            break; // break in order to return to hbmenu
+
+        // Your code goes here
+
+        // Update the console, sending a new frame to the display
+        consoleUpdate(NULL);
+    }
+
+    // Deinitialize and clean up resources used by the console (important!)
+    consoleExit(NULL);
+    return 0;
+}
+
